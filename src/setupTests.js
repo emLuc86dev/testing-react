455f02123a14fef2c0f13671e864fe50ec6c849a
Emilio <emiliodev1986@gmail.com>
2022-07-03 23:03:38 -0400

    ss

--- src/setupTests.js
+++ src/setupTests.js
@@ -4,10 +4,11 @@
 // learn more: https://github.com/testing-library/jest-dom
 // import '@testing-library/jest-dom/extend-expect';
 import Enzyme from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
+import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
 import {createSerializer} from 'enzyme-to-json';
- 
-Enzyme.configure({ adapter: new Adapter() });
 
+Enzyme.configure({ adapter: new Adapter() });
 expect.addSnapshotSerializer(createSerializer({mode: 'deep'}));
+
+
 
